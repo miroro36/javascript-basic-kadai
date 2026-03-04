330dc690ad6b2b36dc9dc62ea6ddc5270978cd55
miroro36 <saburo.yamada.36@gmail.com>
2026-03-04 11:07:10 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,9 @@
 const today = new Date();
 
 const year = today.getFullYear();
-const month = today.getMonth() + 1; // 月は0始まり
+const month = today.getMonth() + 1; 
 const day = today.getDate();
 
-const dateString = `${year}/${month}/${day}`;
+const dateString = `${year}年${month}月${day}日`;
 
 console.log(dateString);
